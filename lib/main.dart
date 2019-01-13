db940ca5afcd9c8f6a25b7ad8da626edd9441e75
GitBoyXuChang <574616156@qq.com>
2019-01-13 18:02:32 +0800

    Initial commit
    Signed-off-by: GitBoyXuChang <574616156@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,28 @@ class _MyHomePageState extends State<MyHomePage> {
                     }));
               },
             ),
+            FlatButton(
+              child: Text("SwitchAndCheckBoxDemo Widget"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.push( context,
+                    new MaterialPageRoute(builder: (context) {
+                      return new SwitchAndCheckBoxTestRoute();
+                    }));
+              },
+            ),
+            FlatButton(
+              child: Text("TextFieldDemo Widget"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.push( context,
+                    new MaterialPageRoute(builder: (context) {
+                      return new TextFieldDemo();
+                    }));
+              },
+            ),
 
           ],
         ),
@@ -178,19 +200,34 @@ class TextStyleDemo extends StatelessWidget {
 }
 
 
-class NewClass extends StatelessWidget {
+class TextFieldDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("New TextStyle"),
+        title: Text("TextFieldDemo"),
       ),
       body: Center(
         child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  labelText: "用户名",
+                  hintText: "用户名或邮箱",
+                  prefixIcon: Icon(Icons.person)
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "密码",
+                  hintText: "您的登录密码",
+                  prefixIcon: Icon(Icons.lock)
+              ),
+              obscureText: true,
+            ),
           ],
         ),
 
@@ -246,7 +283,51 @@ class ButtonDemo extends StatelessWidget {
   }
 }
 
+
+/*
+* 参数
+Image在显示图片时定义了一系列参数，通过这些参数我们可以控制图片的显示外观、大小、混合效果等。我们看一下Image的主要参数：
+
+const Image({
+  ...
+  this.width, //图片的宽
+  this.height, //图片高度
+  this.color, //图片的混合色值
+  this.colorBlendMode, //混合模式
+  this.fit,//缩放模式
+  this.alignment = Alignment.center, //对齐方式
+  this.repeat = ImageRepeat.noRepeat, //重复方式
+  ...
+})
+width、height：用于设置图片的宽、高，当不指定宽高时，图片会根据当前父容器的限制，尽可能的显示其原始大小，如果只设置width、height的其中一个，那么另一个属性默认会按比例缩放，但可以通过下面介绍的fit属性来指定适应规则。
+
+fit：该属性用于在图片的显示空间和图片本身大小不同时指定图片的适应模式。适应模式是在BoxFit中定义，它是一个枚举类型，有如下值：
+
+fill：会拉伸填充满显示空间，图片本身长宽比会发生变化，图片会变形。
+cover：会按图片的长宽比放大后居中填满显示空间，图片不会变形，超出显示空间部分会被剪裁。
+contain：这是图片的默认适应规则，图片会在保证图片本身长宽比不变的情况下缩放以适应当前显示空间，图片不会变形。
+fitWidth：图片的宽度会缩放到显示空间的宽度，高度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
+fitHeight：图片的高度会缩放到显示空间的高度，宽度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
+none：图片没有适应策略，会在显示空间内显示图片，如果图片比显示空间大，则显示空间只会显示图片中间部分。
+*
+*
+*
+* */
 class ImagesDemo extends StatelessWidget {
+
+/*
+  String icons = "";
+// accessible: &#xE914; or 0xE914 or E914
+  // ignore: expected_class_member, missing_function_body, duplicate_definition, missing_const_final_var_or_type, missing_method_parameters, missing_keyword_operator, invalid_operator
+  icons; += "\uE914";
+// error: &#xE000; or 0xE000 or E000
+  // ignore: duplicate_definition, missing_const_final_var_or_type, missing_method_parameters, missing_keyword_operator, invalid_operator, expected_class_member, missing_function_body
+  icons;  += " \uE000";
+// fingerprint: &#xE90D; or 0xE90D or E90D
+  // ignore: duplicate_definition, missing_const_final_var_or_type, missing_method_parameters, missing_keyword_operator, invalid_operator, missing_function_body, expected_class_member, expected_class_member
+  icons; += " \uE90D";
+*/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -258,7 +339,47 @@ class ImagesDemo extends StatelessWidget {
 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            Image(
+                image: AssetImage("images/img1.png"),
+                width: 50.0
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"),
+              width: 50.0,
+            ),
+            Image.network(
+              "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+              width: 50.0,
+            ),
+            Image(
+              image: AssetImage("images/img1.png"),
+              width: 50.0,
+              color: Colors.blue,
+              colorBlendMode: BlendMode.difference,
+            ),
+            Image(
+              image: AssetImage("images/img1.png"),
+              width: 50.0,
+              height: 100.0,
+              repeat: ImageRepeat.repeatY ,
+            ),
+            Icon(Icons.accessible,color: Colors.green,),
+            /*Text(icons,
+              style: TextStyle(
+                  fontFamily: "MaterialIcons",
+                  fontSize: 24.0,
+                  color: Colors.green
+              ),
+            ),*/
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.accessible,color: Colors.green,),
+                Icon(Icons.error,color: Colors.green,),
+                Icon(Icons.fingerprint,color: Colors.green,),
+              ],
+            )
           ],
         ),
 
@@ -267,27 +388,40 @@ class ImagesDemo extends StatelessWidget {
   }
 }
 
-class SwitchAndCheckBoxDemo extends StatelessWidget {
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() => new _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected=true; //维护单选开关状态
+  bool _checkboxSelected=true;//维护复选框状态
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("SwitchAndCheckBoxDemo"),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected,//当前状态
+          onChanged:(value){
+            //重新构建页面
+            setState(() {
+              _switchSelected=value;
+            });
+          },
         ),
-
-      ),
+        Checkbox(
+          value: _checkboxSelected,
+          activeColor: Colors.red, //选中时的颜色
+          onChanged:(value){
+            setState(() {
+              _checkboxSelected=value;
+            });
+          } ,
+        )
+      ],
     );
   }
 }
-
 
 class Echo extends StatelessWidget {
   const Echo({
